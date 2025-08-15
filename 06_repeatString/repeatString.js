7e98ce8d135bab0d8ec55f1ee09ab879d82127e5
CondurachiCristian <107665724+CondurachiCristian@users.noreply.github.com>
2025-08-15 13:59:23 +0300

    Complete exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    let result = '';
+    for (let i = 0; i < times; i++) {
+        result += str;
+    } 
+    if (times < 0) {
+        result = 'ERROR'
+    }
+    return result;
 };
 
 // Do not edit below this line
