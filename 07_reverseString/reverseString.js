0d34397ab929add1fa0c2bcc536c2d90b90e0a9b
CondurachiCristian <107665724+CondurachiCristian@users.noreply.github.com>
2025-08-15 14:15:54 +0300

    Complete exercise 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arr = Array.from(str);
+    let revArr = arr.reduce((acc, current) => 
+    [current, ...acc], []);
+    revStr = revArr.join('');
+    return revStr
 };
 
 // Do not edit below this line
