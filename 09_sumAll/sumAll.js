529c6f7f83606c924c29ba259365b027f077c2fe
CondurachiCristian <107665724+CondurachiCristian@users.noreply.github.com>
2025-08-16 13:48:10 +0300

    Complete exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+   if (Number.isInteger(firstNumber) && Number.isInteger(secondNumber) && firstNumber < secondNumber && firstNumber > 0 && secondNumber > 0) {
+    let arr = [];
+    while (firstNumber <= secondNumber) {
+       arr.push(firstNumber)
+       firstNumber++
+    }
+    const initialValue = 0;
+    const sumWithInitial = arr.reduce(
+        (accumulator, currentValue) => accumulator + currentValue, initialValue,
+    );
+    return sumWithInitial
+   } else if (firstNumber > secondNumber) {
+    let arr = [];
+    while (secondNumber <= firstNumber) {
+       arr.push(secondNumber)
+       secondNumber++
+    }
+    const initialValue = 0;
+    const sumWithInitial = arr.reduce(
+        (accumulator, currentValue) => accumulator + currentValue, initialValue,
+    );
+    return sumWithInitial
+   } else {
+    return "ERROR"
+   }
 };
 
 // Do not edit below this line
